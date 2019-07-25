5069984c25fb5b15801474bf37cc694c425dd42d
jussimar <jussimar@jussimarleal.com.br>
2019-07-26 03:04:33 +0900

    inserindo o calculo da soma

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,7 @@
 // This is a JavaScript file
-
+$(document).on("click","#btnSomar",function(){
+    var valor1 = $("#valor1").val();
+    var valor2 = $("#valor2").val();
+    var result = parseFloat(valor1) + parseFloat(valor2);
+   $("#resultado").val(result);
+});
